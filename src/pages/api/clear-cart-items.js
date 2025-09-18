b9eae36ef88ace2ef9d29afe6126699771896abc
Zhanybek <114061193+zhanikpanik@users.noreply.github.com>
2025-09-18 18:29:22 +0600

    fixing postgres setup

--- src/pages/api/clear-cart-items.js
+++ src/pages/api/clear-cart-items.js
@@ -1,4 +1,4 @@
-import pool from '../../../lib/db.js';
+import pool from '../../lib/db.js';
 
 export const prerender = false;
 
--- src/pages/api/get-cart-items.js
+++ src/pages/api/get-cart-items.js
@@ -1,4 +1,4 @@
-import pool, { initializeDb } from '../../../lib/db.js';
+import pool, { initializeDb } from '../../lib/db.js';
 
 export const prerender = false;
 
